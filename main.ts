358f4e0f905cf99a3fffa096bcbb92b56d786e76
MYNAMEISSKYLARWHITEYO <116825276+MYNAMEISSKYLARWHITEYO@users.noreply.github.com>
2022-10-31 11:16:48 -0400

    f

--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+})
 basic.forever(function () {
 	
 })
